211ff08bc3d33ead0919024326534132d92b61f2
Jipu Wang <jipuwang@umich.edu>
2016-12-05 15:58:27 -0500

    add linearizer header

---
+++ linearizer.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include <vector>
+// THis is the function used to 
+// todo: change the int elementaryFunction to a enumType
+void linearizeCurrent(int elementaryFunctionType, double lb, double ub, 
+        int meshPts, aCurrent, bCurrent);
+:
